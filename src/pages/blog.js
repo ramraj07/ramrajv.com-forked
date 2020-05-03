58cd4b057b7b921fb4b723ebacb7d4af64313042
Ramraj Velmurugan <ramraj.velmurugan@komodohealth.com>
2020-05-04 04:35:40 +0530

    Change names and links erverywhere

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -40,13 +40,13 @@ function blog({ data: { allMarkdownRemark: { edges } } }) {
 
   return (
     <section className={CSS.blog}>
-      <HeadTag title="Blog Posts" description="Blog index for NoahYamamoto.com" path="/blog" />
+      <HeadTag title="Blog Posts" description="Blog index for ramrajv.com" path="/blog" />
       <Canvas />
       <header className={CSS.blogHeader}>
         <div className={CSS.topHeader}>
           <div className={CSS.nameContainer}>
             <FancyLink to="/" internal animated>
-              Noah Yamamoto
+              Ramraj Velmurugan
             </FancyLink>
           </div>
         </div>
